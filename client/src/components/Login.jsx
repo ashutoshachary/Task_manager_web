bd87aad8f6c551d08ca75d795f176199196ec407
ashutoshachary <111655759+ashutoshachary@users.noreply.github.com>
2024-10-02 19:18:57 +0530

    Update Login.jsx
    chnaged the ip for the render

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const res = await axios.post('http://100.20.92.101:5000/api/auth/login', { email, password });
       localStorage.setItem('token', res.data.token);
       window.location.href = '/dashboard';
     } catch (err) {
